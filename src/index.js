30f055689a33a15a2197caab744b1e054c3dfb88
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2023-02-12 23:31:13 +0530

    fnish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-const express=require('express');
-const mongoose = require('mongoose');
-const route = require('./router/router');
-const app=express();
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.json());
+const events = [
+  { text: 'textOne', dateTime: '2020-07-10 15:00:00.000' },
+  { text: 'textTwo', dateTime: '2020-07-11 16:00:00.000' },
+  { text: 'textThree', dateTime: '2020-07-12 17:00:00.000' },
+  { text: 'textFour', dateTime: '2020-07-13 18:00:00.000' },
+  { text: 'textFive', dateTime: '2020-07-14 19:00:00.000' },
+  { text: 'textSix', dateTime: '2020-07-15 20:00:00.000' },
+  { text: 'textSeven', dateTime: '2020-07-16 21:00:00.000' },
+  { text: 'textEight', dateTime: '2020-07-17 22:00:00.000' },
+  { text: 'textNine', dateTime: '2020-07-18 23:00:00.000' },
+  { text: 'textTen', dateTime: '2020-07-19 00:00:00.000' },
+];
 
-mongoose.set('strictQuery', false)
-mongoose.connect("mongodb+srv://Avverma:Avverma95766@avverma.2g4orpk.mongodb.net/backend", {
-    useNewUrlParser: true,
-}).then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+app.get('/schedule', (req, res) => {
+  events.forEach((event) => {
+    setTimeout(() => {
+      console.log(event.text.split('').reverse().join(''));
+    }, event.text.length * 1000);
+  });
+  res.send('Events have been scheduled');
+});
 
-
-app.use('/',route);
-let PORT=3000
-app.listen(PORT, () => console.log(`server port is  ${PORT}`));
+app.listen(port, () => {
+  console.log(`Scheduler app listening at http://localhost:${port}`);
+});
