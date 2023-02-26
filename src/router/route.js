47ed7eb047549c22f4541814969bcd8869575744
AnkushRai-222 <ankushrai222@gmail.com>
2023-02-26 23:17:53 +0530

    Done

--- src/router/route.js
+++ src/router/route.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router()
 const itinerarycontroller = require('../controller/itineraryController')
 
-router.post("/student",itinerarycontroller.createItinerary)
-router.get("/student",itinerarycontroller.getItinerary)
-router.delete('/student/:studentId',itinerarycontroller.deleteItinerary)
+router.post("/createItinerary",itinerarycontroller.createItinerary)
+router.get("/getItinerary",itinerarycontroller.getItinerary)
+router.delete('deleteItinerary',itinerarycontroller.deleteItinerary)
+router.put('updateItinerary',itinerarycontroller.updateItinerary)
 
 module.exports = router;
