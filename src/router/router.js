875fa69b4803693376e5b3873562c716f31cfd83
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2023-03-20 10:30:21 +0530

    route added

--- src/router/router.js
+++ src/router/router.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const router = express.Router()
-const {createUser ,UpdateUser,userLogIn} = require('../controller/usercontroller')
-const {createSlot ,bookSlot} = require('../controller/slotsController')
-// const {userDetails, loginAdmin,slotDetails,updateAdmin} = require('../controller/adminController')
-// const { authenticate, authorization } = require('../middleware/AdminAuthentication')
-const { UserAuthentication, UserAuthorization } = require('../middleware/userAuthentication')
+const {createRound} = require('../controller/usercontroller')
+
  
-router.post("/register", createUser)
-router.put('/user/:userId/profile',UserAuthentication,UserAuthorization,UpdateUser)
-router.get('/logIn',userLogIn)
+router.post("/createRound", createRound)
 
-router.post('/registerSlot',createSlot)
-router.put('/bookSlots/:userId',bookSlot)
-
-// router.get('/admin/getUserDetails',userDetails)
-// router.get('/admin/login',loginAdmin)
-// router.get('/admin/slotDetails',slotDetails)
-// router.put('/admin/details',authenticate,authorization,updateAdmin)
 
 module.exports = router;
